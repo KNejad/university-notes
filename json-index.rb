b877de79941598d21d6196c41d45873db4d52f38
Keeyan Nejad <keeyan@knejad.co.uk>
2017-03-09 21:46:38 +0000

    Updated json-index to arrange files better and moved titles.sh to json-titles.sh

--- json-index.rb
+++ json-index.rb
@@ -1,19 +1,32 @@
 #!/usr/bin/ruby -w
 
+
+
 require "json"
-def directory_hash(course)  
-	directory =  Dir[course + "/**/*.json"].sort
-	
+
+def containsJSON(directory) 
+	return !(Dir[directory + "/**/*.json"].empty?)
+end
+
+def directory_hash(directory) 
+
+	directories =  Dir.entries(directory).sort
 	tree = Hash.new
-	tree["_title"] = course.upcase + " Index"
-	tree["_date"] =  Time.now
-	directory.each do |file|
-		jsonFile = JSON.load(open(file))
-		title = jsonFile["_title"]
-		tree[title] = Hash.new
-		tree[title]["_href"] = file
+
+	directories.each do |entry|
+		address = directory + "/" + entry
+		if entry == "." or  entry == ".." then
+			next
+		elsif File.directory?(address) and containsJSON(address) then
+			tree[entry.capitalize] = directory_hash(directory + "/" +  entry)
+		elsif entry.end_with? ".json" then
+			jsonFile = JSON.load(open(address))
+			title = jsonFile["_title"]
+			tree[title] = Hash.new
+			tree[title]["_href"] = address
+		end
 	end
-	return JSON.pretty_generate(tree)
+	return tree
 end
 
 
@@ -23,5 +36,10 @@ courses = ["cs2506", "cs2510", "cs2512", "cs2521"]
 courses.each do |course|
 	file = course +  '/index.json'
 	File.delete(file) if File.exist?(file)
-	File.write(file ,directory_hash(course))
+	courseTree = Hash.new
+	courseTree["_title"] = course.upcase + " Index"
+	courseTree["_date"] =  Time.now
+	courseTree.merge!(directory_hash(course))
+
+	File.write(file ,JSON.pretty_generate(courseTree))
 end
