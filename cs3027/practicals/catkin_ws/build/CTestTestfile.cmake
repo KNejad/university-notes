f06bdab2ab3016cdb84dd33d942ff99dae8cd073
Keeyan Nejad <keeyan@keeyan.xyz>
2018-09-25 15:43:48 +0100

    Finished first ROS practical with all code in this repo

---
+++ cs3027/practicals/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/src
+# Build directory: /media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("hello")
+subdirs("practical_01_05")
---
+++ cs3027/practicals/catkin_ws/build/practical_01_05/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "practical_01_05"
+PROJECT_SPACE_DIR = "/media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/devel"
+PROJECT_VERSION = "0.0.0"
