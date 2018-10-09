310874523e65481a6cde76f54e64ca07da120243
Keeyan Nejad <keeyan@keeyan.xyz>
2018-10-09 17:08:41 +0100

    Don't track devel and make in catkin_ws

--- cs3027/practicals/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/devel;/opt/ros/melodic".split(';') if "/media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/devel;/opt/ros/melodic" != "" else []
--- cs3027/practicals/catkin_ws/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /media/sf_host_home/projects/university_notes/cs3027/practicals/catkin_ws/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
