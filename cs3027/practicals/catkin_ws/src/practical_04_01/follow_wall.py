2ca46bd718d44d0d203a99311fb19dc3a913810f
Keeyan Nejad <keeyan@keeyan.xyz>
2018-10-21 14:04:29 +0100

    Keep obstacles instead of distances in practical 04 01

--- cs3027/practicals/catkin_ws/src/practical_04_01/follow_wall.py
+++ cs3027/practicals/catkin_ws/src/practical_04_01/follow_wall.py
@@ -10,54 +10,48 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Header
 
-
 class FollowWall:
+    obstacles = { "left": False, "ahead": False, "all": False }
 
     def __init__(self):
         rospy.init_node("follow_wall", anonymous=True)
 
         rospy.Subscriber("/base_scan", LaserScan, self.laser_scanner_callback)
 
-        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-
         self.tf_listener = tf.TransformListener()
 
-        self.obstacle_positions = { "left": math.inf, "ahead": math.inf, "all": math.inf }
+        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
         self.rate = rospy.Rate(5)
 
     def laser_scanner_callback(self, laser_scan):
-        new_positions = { "left": math.inf, "ahead": math.inf, "all": math.inf }
+        new_distances = { "left": math.inf, "ahead": math.inf, "all": math.inf }
+        new_obstacles = { "left": False, "ahead": False, "all": False }
         current_angle = laser_scan.angle_min
+
         for laser_range in laser_scan.ranges:
             if laser_scan.range_min < laser_range < laser_scan.range_max:
-                obstacle = self.convert_laser_range_to_point(laser_range, current_angle)
-                obstacle_distance = self.euclidean_distance(obstacle.point)
+                obstacle = convert_laser_range_to_point(laser_range, current_angle)
+                obstacle_distance = euclidean_distance(obstacle)
 
-                new_positions["all"] = min(new_positions["all"], obstacle_distance)
+                if  obstacle_distance < new_distances["all"]:
+                    new_obstacles["all"] = obstacle
+                    new_distances["all"] = obstacle_distance
 
-                if -0.5 < current_angle < 0.5:
-                    new_positions["ahead"] = min(new_positions["ahead"], obstacle_distance)
+                if -0.5 < current_angle < 0.5 and obstacle_distance < new_distances["ahead"]:
+                    new_obstacles["ahead"] = obstacle
+                    new_distances["ahead"] = obstacle_distance
 
-                if 1 < current_angle < 1.5:
-                    new_positions["left"] = min(new_positions["left"], obstacle_distance)
+                if 1 < current_angle < 1.5 and obstacle_distance < new_distances["left"]:
+                    new_obstacles["left"] = obstacle
+                    new_distances["left"] = obstacle_distance
 
             current_angle += laser_scan.angle_increment;
 
-        self.obstacle_positions = new_positions
-
-    def convert_laser_range_to_point(self, laser_range, current_angle):
-        x = laser_range*math.cos(current_angle)
-        y = laser_range*math.sin(current_angle)
-        return PointStamped(
-                header=Header(stamp=rospy.Time.now(), frame_id="/base_link"),
-                point=Point(x, y, 0))
-
-    def euclidean_distance(self, a, b=Point(0,0,0)):
-        return math.sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))
+        self.obstacles = new_obstacles
 
     def forward_till_wall(self):
-        while self.obstacle_positions["all"] > 3:
+        while euclidean_distance(self.obstacles["all"]) > 3:
             vel_msg = Twist()
             vel_msg.linear.x = 1
             self.velocity_publisher.publish(vel_msg)
@@ -66,14 +60,14 @@ class FollowWall:
     def follow_wall(self):
         while True:
             vel_msg = Twist()
-            vel_msg.linear.x = self.obstacle_positions["all"]
+            vel_msg.linear.x = euclidean_distance(self.obstacles["all"])
 
-            if self.obstacle_positions["left"] < 1:
+            if euclidean_distance(self.obstacles["left"]) < 1:
                 vel_msg.angular.z = 0
             else:
                 vel_msg.angular.z = 1
 
-            if self.obstacle_positions["ahead"] < 1:
+            if euclidean_distance(self.obstacles["ahead"]) < 1:
                 # Turn right
                 vel_msg.linear.x = 0
                 vel_msg.angular.z = -1
@@ -81,6 +75,21 @@ class FollowWall:
             self.velocity_publisher.publish(vel_msg)
             self.rate.sleep()
 
+def euclidean_distance(a, b=Point(0,0,0)):
+    if a == False or b == False:
+        return math.inf
+
+    if type(a).__name__ == "PointStamped":
+        a = a.point
+
+    return math.sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))
+
+def convert_laser_range_to_point(laser_range, current_angle):
+    x = laser_range*math.cos(current_angle)
+    y = laser_range*math.sin(current_angle)
+    return PointStamped(
+            header=Header(stamp=rospy.Time.now(), frame_id="/base_link"),
+            point=Point(x, y, 0))
 
 if __name__ == "__main__":
     try:
